7381abc02c73224c0638a5cfbaddc2e4a0692c7a
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-05-10 17:30:51 -0300

    Setting up environment variables.

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Layout from '../core/Layout'
+import {API} from '../config'
 
 const Signup = () => {
     return(
         <Layout
             title='Signup'
             description='Signup to Node React E-commerce App'>
-            ...
+            {API}
         </Layout>
     );
 };
