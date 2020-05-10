118ff90eb9e33ab5227f188b1f146ddc81ed84d3
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-05-10 13:38:07 -0300

    Created navigation menu.

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './core/Home';
+import Menu from './core/Menu';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
 
 const Routes = () => {
     return(
-        <div>
-            <BrowserRouter>
-                <Route 
-                    path='/'
-                    exact component={Home}/>            
-                <Route 
-                    path='/signin'
-                    exact component={Signin}/>
-                <Route 
-                    path='/signup'
-                    exact component={Signup}/>                    
-            </BrowserRouter>
-        </div>
+        <BrowserRouter>
+            <Menu />
+            <Route 
+                path='/'
+                exact component={Home}/>            
+            <Route 
+                path='/signin'
+                exact component={Signin}/>
+            <Route 
+                path='/signup'
+                exact component={Signup}/>                    
+        </BrowserRouter>
     );
 };
 
