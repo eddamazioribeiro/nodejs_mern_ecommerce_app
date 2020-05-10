b6c216ea25173dcfddba4bb0275f7cfd8ce080be
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-05-09 21:57:33 -0300

    Created user initial components (sing-in and sign-up).

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  return(
-    <div>
-      <h2>Hello React!</h2>
-    </div>
-  );
+    return (
+        <div>
+          <h2>Hello React!</h2>
+        </div>
+    );
 };
 
 export default App;
---
+++ src/user/Signin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Signin = () => {
+    return(
+        <div>
+            <h2>Signin</h2>
+        </div>
+    );
+};
+
+export default Signin;
