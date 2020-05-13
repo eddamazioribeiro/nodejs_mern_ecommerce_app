d38cafbfd0b3d003e1a2003ea9e176de205187a6
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-05-12 23:07:48 -0300

    Creating SignUp page, handling input values using useState hook.

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,15 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Layout from '../core/Layout'
 import {API} from '../config'
 
 const Signup = () => {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success: false
+    });
+
+    const handleChange = name => event => {
+        setValues({...values, error: false, [name]: event.target.value})
+    }; 
+
+    const signUpForm = () => {
+        return(
+            <form>
+            <div className='form-group'>
+                <label
+                    className='text-muted'>Name
+                </label>
+                <input
+                    type='text'
+                    className='form-control'
+                    onChange={handleChange('name')}>
+                </input>
+            </div>
+            <div className='form-group'>
+                <label
+                    className='text-muted'>E-mail
+                </label>
+                <input
+                    type='email'
+                    className='form-control'
+                    onChange={handleChange('email')}>
+                </input>
+            </div>
+            <div className='form-group'>
+                <label
+                    className='text-muted'>Password
+                </label>
+                <input
+                    type='password'
+                    className='form-control'
+                    onChange={handleChange('password')}>
+                </input>
+            </div>
+            <button className='btn btn-primary'>Submit
+            </button>                     
+        </form>            
+        );
+    }
+
     return(
         <Layout
             title='Signup'
-            description='Signup to Node React E-commerce App'>
-            {API}
+            description='Signup to Node React E-commerce App'
+            className='container col-md-8 offset-mde-2'>
+            {signUpForm()}
+            {JSON.stringify(values)}
         </Layout>
     );
-};
+}
 
 export default Signup;
