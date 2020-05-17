e0a8f77df7e230ff17a16f9808d4439af3f37a19
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-05-17 18:03:16 -0300

    Implemented sign-in.

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,10 +1,29 @@
 import {API} from '../config'
 
 export const signUp = (user) => {
-    console.log(user);
 
     return(
         fetch(`${API}/signup`,{
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => {
+            return res.json();
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    );
+}
+
+export const signIn = (user) => {
+
+    return(
+        fetch(`${API}/signin`,{
             method: "POST",
             headers: {
                 Accept: "application/json",
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,14 +1,119 @@
-import React from 'react';
-import Layout from '../core/Layout'
+import React, {useState} from 'react';
+import {Redirect} from 'react-router-dom';
+import Layout from '../core/Layout';
+import {signIn} from '../auth';
 
-const Signin = () => {
+const SignIn = () => {
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        error: '',
+        loading: false,
+        redirectToReferrer: false
+    });
+
+    const {email, password, error, loading, redirectToReferrer} = values;
+
+    const handleChange = name => event => {
+        setValues({...values, error: false, [name]: event.target.value})
+    }
+    
+    const clickSubmit = (event) => {
+        event.preventDefault();
+
+        setValues({...values, loading: true});
+
+        signIn({email, password})
+            .then(data => {
+                if (data.error) {
+                    setValues({
+                        ...values,
+                        error: data.error,
+                        loading: false
+                    });
+                } else {
+                    setValues({
+                        ...values,
+                        redirectToReferrer: true,
+                        loading: false
+                    });
+                }
+            }) ;
+    }
+
+    const signInForm = () => {
+        return(
+            <form>
+            <div className='form-group'>
+                <label
+                    className='text-muted'>E-mail
+                </label>
+                <input
+                    type='email'
+                    className='form-control'
+                    onChange={handleChange('email')}
+                    value={email}>
+                </input>
+            </div>
+            <div className='form-group'>
+                <label
+                    className='text-muted'>Password
+                </label>
+                <input
+                    type='password'
+                    className='form-control'
+                    onChange={handleChange('password')}
+                    value={password}>
+                </input>
+            </div>
+            <button
+                onClick={clickSubmit}
+                className='btn btn-primary'>Submit
+            </button>                     
+        </form>            
+        );
+    }
+
+    const showError = () => {
+        return(
+            <div
+                className='alert alert-danger'
+                style={{display: error ? '' : 'none'}}>
+                    {error}
+            </div>
+        );
+    }
+
+    const showLoading = () => {
+        if (loading) {
+            return (
+                <div
+                    className='alert alert-info'>
+                        <h2>Loading...</h2>
+                </div>
+            );
+        }
+    }
+
+    const redirectUser = () => {
+        if (redirectToReferrer) {
+            return(
+                <Redirect to="/"/>
+            );
+        }
+    }
+
     return(
         <Layout
-            title='Signin'
-            description='Signin to Node React E-commerce App'>
-            ...
+            title='Signup'
+            description='Signup to Node React E-commerce App'
+            className='container col-md-8 offset-mde-2'>
+            {showLoading()}
+            {showError()}
+            {signInForm()}
+            {redirectUser()}
         </Layout>
     );
-};
+}
 
-export default Signin;
+export default SignIn;
