f14993cf2c14b3473ae6d5cfecefa3b99d22e2f5
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-05-28 23:49:06 -0300

    Applying some styling on home page (css).

--- src/core/Card.js
+++ src/core/Card.js
@@ -19,7 +19,7 @@ const Card = ({product}) => {
                 <p>{product.price}</p>
                 <Link to='/'>
                     <button
-                        className='btn btn-outline-primary mt-2 mb-2'>
+                        className='btn btn-outline-primary mt-2 mb-2 mr-2'>
                             View product
                     </button>
                     <button
