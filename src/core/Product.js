ed332d4a70508a791eb2621b42587810a2a3653d
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-06-13 20:06:51 -0300

    Fixed setProduct error when retrieving product from backend.

--- src/core/Product.js
+++ src/core/Product.js
@@ -18,15 +18,18 @@ const Product = (props) => {
             if (data.error) {
                 setError(data.error);
             } else {
-                setProduct(data);
+                setProduct(data.product);
             }
         });
     }
 
     return(
         <Layout
-            title='Home Page'
-            description='Node React E-commerce App'
+            title={product && product.name}
+            description={
+                product
+                && product.description
+                && product.description.substring(0, 100)}
             className='container-fluid'>
             <h2 className='mb-4'>Single Product</h2>
             <div className='row'>
