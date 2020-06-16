0eb9fd3c5aed4fe44ea212b6f13dcbfdb30af65b
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-06-15 22:53:18 -0300

    Showing detailed product data on Card component and applying some style.

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import {Link} from 'react-router-dom';
 import ShowImage from './ShowImage';
 
@@ -18,10 +19,29 @@ const Card = ({product, showViewProductButton = true}) => {
         );
     }
 
+    const showAddToCartButton = () => {
+        return(
+            <p className='btn btn-outline-warning mt-2 mb-2'>
+                Added to cart
+            </p>  
+        );
+    }
+
+    const showStock = (quantity) => {
+        let message =
+            (quantity > 0) ? `In stock: ${quantity}` : 'Out of stock'
+
+        return quantity > 0 && (
+            <span className='badge badge-primary badge-pill'>
+                {message}
+            </span>
+        );
+    }
+
     return(
         <div className='card'>
             <div
-                className='card-header'>
+                className='card-header name'>
                 {product.name}
             </div>
             <div
@@ -29,13 +49,19 @@ const Card = ({product, showViewProductButton = true}) => {
                 <ShowImage
                     item={product}
                     url="product"/>
-                <p>{product.description.substring(0, 100)}</p>
-                <p>{product.price}</p>
+                <p className='lead mt-2'>{product.description.substring(0, 100)}</p>
+                <p className='black-10'>
+                    $ {product.price}</p>
+                <p className='black-9'>
+                    Category: {product.category && product.category.name}
+                </p>
+                <p className='black-8'>
+                    Added on {moment(product.createdAt).fromNow()}
+                </p>
+                {showStock(product.quantity)}
+                <br/>             
                 {showViewButton(showViewProductButton)}               
-                <button
-                    className='btn btn-outline-warning mt-2 mb-2'>
-                        Add to cart
-                </button>                  
+                {showAddToCartButton()}            
             </div>
         </div>
 
