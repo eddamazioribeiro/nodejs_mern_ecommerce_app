beffd808d149fae1fca53726ee9ae8c0c5c43838
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-06-19 15:44:21 -0300

    Code improvement on cartHelpers.

--- src/core/cartHelpers.js
+++ src/core/cartHelpers.js
@@ -1,63 +1,56 @@
-export const getCart = () => {
-    let cart = [];
+export const getLocalStorageItem = (itemName) => {
+    let item = [];
     
     if (typeof window !== 'undefined') {
-        if (localStorage.getItem('cart')) {
-            cart = JSON.parse(localStorage.getItem('cart'));
+        if (localStorage.getItem(itemName)) {
+            item = JSON.parse(localStorage.getItem(itemName));
         }
     }
 
-    return cart;
+    return item;
+}
+
+export const getCart = () => {
+    let cart = getLocalStorageItem('cart');
+
+    return cart ? cart : 'undefined';
 }
 
 export const updateItem = (productId, count) => {
-    let cart = [];
-    
-    if (typeof window !== 'undefined') {
-        if (localStorage.getItem('cart')) {
-            cart = JSON.parse(localStorage.getItem('cart'));
-        }
+    let cart = getCart();
 
+    if (cart) {
         cart.map((p, i) => {
             if (p._id === productId) {
                 cart[i].count = count;
             }
         });
-
+    
         localStorage.setItem('cart', JSON.stringify(cart));
     }
-
 }
 
 export const addItem = (item, next) => {
-    let cart = [];
+    let cart = getCart();
 
-    if (typeof window !== 'undefined') {
-        if (localStorage.getItem('cart')) {
-            cart = JSON.parse(localStorage.getItem('cart'));
-        }
-
+    if (cart) {
         cart.push({...item,
             count: 1
         });
-
+    
         cart = Array.from(new Set(cart.map((p) => (p._id))))
             .map((id) => {
                 return cart.find(p => p._id === id);
             });
-
+    
         localStorage.setItem('cart', JSON.stringify(cart));
-
+    
         next();
     }
 }
 
 export const itemTotal = () => {
-    if (typeof window !== 'undefined') {
-        if (localStorage.getItem('cart')) {
-            return JSON.parse(localStorage.getItem('cart')).length;
-        }
-    }
+    let cartLength = getCart() ? getCart().length : 0;
 
-    return 0;
+    return cartLength;
 }
