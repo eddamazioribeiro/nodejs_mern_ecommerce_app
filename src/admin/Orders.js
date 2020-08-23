274f7213440e55a2cb94febe19aeb71bb0b10907
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-23 00:45:11 -0300

    Listing order info in Order component.

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -3,6 +3,7 @@ import Layout from '../core/Layout';
 import {isAuthenticated} from '../auth';
 import {Link} from 'react-router-dom';
 import {listOrders} from './apiAdmin';
+import moment from 'moment';
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
@@ -23,8 +24,20 @@ const Orders = () => {
         loadOrders();
     }, []);
 
-    const noOrders = orders => {
-        return orders.length < 1 ? <h4>No orders</h4> : null;
+    const showOrdersLength = () => {
+        if (orders.length > 0) {
+            return(
+                <h1 className='text-danger display-2'>
+                    Total orders: {orders.length}
+                </h1>
+            );
+        } else {
+            return(
+                <h1 className='text-danger'>
+                    No orders
+                </h1>
+            );
+        }
     }
 
     return(
@@ -33,8 +46,44 @@ const Orders = () => {
             description={`Hello, ${user.name}! You can manage all the orders here`}>
             <div className='row'>
                 <div className='col-md-8 offset-md-2'>
-                    {noOrders(orders)}
-                    {JSON.stringify(orders)}
+                    {showOrdersLength()}
+                    {orders.map((order, i) => {
+                        return(
+                            <div
+                                key={i}
+                                className='mt-5'
+                                style={{borderBottom: '5px solid indigo'}}>
+                                <h2 className='mb-5'>
+                                    <span className='bg-primary'>
+                                        Order ID: {order._id}
+                                    </span>
+                                </h2>
+                                <ul>
+                                    <li className='list-group-item'>
+                                        {order.status}
+                                    </li>
+                                    <li className='list-group-item'>
+                                        Transaction ID: {order.transaction_id}
+                                    </li>
+                                    <li className='list-group-item'>
+                                        Amount: ${order.amount}
+                                    </li>
+                                    <li className='list-group-item'>
+                                        Ordered by: {order.user.name}
+                                    </li>
+                                    <li className='list-group-item'>
+                                        Ordered on: {moment(order.createdAt).format('DD/MM/yyyy')}, {moment(order.createdAt).fromNow()}
+                                    </li>
+                                    <li className='list-group-item'>
+                                        Delivered address: {order.address}
+                                    </li>                                                                                                                                                                     
+                                </ul>
+                                <h3 className='mt-4 mb-4 font-italic'>
+                                    Total products in the order: {order.products.length}
+                                </h3>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </Layout>
