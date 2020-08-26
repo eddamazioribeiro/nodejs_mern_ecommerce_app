9140b431cc815b8e60f7fbc06c55ce11a9925a48
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-25 23:20:57 -0300

    Using user resource to update user's information via Profile page.

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from '../core/Layout';
 import {isAuthenticated} from '../auth';
 import {Link} from 'react-router-dom';
+import {read, update, updateUser} from './apiUser'
 
-const Profile = () => {
+const Profile = ({match}) => {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: false,
+        success: false
+    });
+
+    const {token} = isAuthenticated();
+
+    const {name, email, password, error, success} = values
+
+    const init = (userId) => {
+        // console.log('user id', userId);
+        read(userId, token)
+        .then(data => {
+            if (data.error) {
+                setValues({...values, error: true})
+            } else {
+                setValues({...values,
+                    name: data.name,
+                    email: data.email
+                });
+            }
+        });
+    }
+
+    useEffect(() => {
+        init(match.params.userId);
+    }, []);
+
     return(
-        <h2>
-            Profile
-        </h2>
+        <Layout
+            title='Profile'
+            description='Update your profile information'
+            className='container-fluid'>
+            <h2 className='mb-4'>
+                Profile update
+            </h2>
+            {JSON.stringify(values)}       
+        </Layout>
     );
 };
 
