c51a935ed1061b9020d1d081188c12daa0998e9b
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-26 21:47:00 -0300

    Create form to update user's information on Profile page.

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Layout from '../core/Layout';
 import {isAuthenticated} from '../auth';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import {read, update, updateUser} from './apiUser'
 
 const Profile = ({match}) => {
@@ -36,6 +36,83 @@ const Profile = ({match}) => {
         init(match.params.userId);
     }, []);
 
+    const handleChange = name = e => {
+        setValues({...values,
+            error: false,
+            [name] : e.target.value});
+    }
+
+    const clickSubmit = (e) => {
+        e.preventDefault();
+        
+        update(match.params.userId, token, {name, email, password})
+        .then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                updateUser(data, () => {
+                    setValues({...values, 
+                        name: data.name,
+                        email: data.email,
+                        success: true
+                    });
+                })
+            }
+        })
+    }
+
+    const redirectUser = (success) => {
+        if (success) {
+            return(
+                <Redirect to='cart'/>
+            );
+        }
+    }
+
+    const profileUpdate = (name, email, password) => {
+        return(
+            <form>
+                <div className='form-group'>
+                    <label className='text-muted'>
+                        Name
+                    </label>
+                    <input
+                        type='text'
+                        className='form-control'
+                        value={name}
+                        onChange={handleChange('name')}>
+                    </input>
+                </div>
+                <div className='form-group'>
+                    <label className='text-muted'>
+                        Email
+                    </label>
+                    <input
+                        type='email'
+                        className='form-control'
+                        value={email}
+                        onChange={handleChange('email')}>
+                    </input>
+                </div>
+                <div className='form-group'>
+                    <label className='text-muted'>
+                        Password
+                    </label>
+                    <input
+                        type='password'
+                        className='form-control'
+                        value={password}
+                        onChange={handleChange('password')}>
+                    </input>
+                </div>
+                <button
+                    onClick={clickSubmit()}
+                    className='btn btn-primary'>
+                </button>                            
+            </form>
+        );
+    }
+
     return(
         <Layout
             title='Profile'
@@ -44,7 +121,8 @@ const Profile = ({match}) => {
             <h2 className='mb-4'>
                 Profile update
             </h2>
-            {JSON.stringify(values)}       
+            {profileUpdate(name, email, password)}
+            {redirectUser(success)}
         </Layout>
     );
 };
