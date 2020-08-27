19f8e968f1dda3bd8faa65b80e4c3943a931c2bc
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-26 22:22:59 -0300

    Showing purcase history (orders) on User Dashboard.

--- src/user/apiUser.js
+++ src/user/apiUser.js
@@ -48,3 +48,20 @@ export const updateUser = (user, next) => {
         }
     }
 }
+
+export const getPurchaseHistory = (userId, token) => {
+    return fetch(`${API}/orders/by/user/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+    .then(res => {
+        return res.json();
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
