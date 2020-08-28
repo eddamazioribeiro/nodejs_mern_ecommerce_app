69c60fb4c6c2d3c12840f99ecc698af25bd15008
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-27 23:30:12 -0300

    Implemented list products and delete on Manage Product page.

--- src/admin/ManageProducts.js
+++ src/admin/ManageProducts.js
@@ -2,8 +2,37 @@ import React, {useState, useEffect} from 'react';
 import Layout from '../core/Layout';
 import {isAuthenticated} from '../auth';
 import {Link} from 'react-router-dom';
+import {getProducts, deleteProduct} from '../admin/apiAdmin';
 
 const ManageProducts = () => {
+    const [products, setProducts] = useState([]);
+    const {user, token} = isAuthenticated();
+
+    useEffect(() => {
+        loadProducts();
+    }, []);
+
+    const loadProducts = () => {
+        getProducts()
+        .then(data => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                setProducts(data);
+            }
+        });
+    };
+
+    const destroy = (productId) => {
+        deleteProduct(productId, user._id, token)
+        .then(data => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                loadProducts();
+            }
+        });
+    }
 
     return(
         <Layout
@@ -11,8 +40,33 @@ const ManageProducts = () => {
             description='Create, update or delete Products'
             className='container-fluid'>
             <div className='row'>
-                <div>
-                    ...
+                <div className='col-12'>
+                    <ul className='list-group'>
+                        {products.map((prod, i) => {
+                            return(
+                                <li
+                                    key={i}
+                                    className='list-group-item d-flex justify-content-between align-items-center'>
+                                    <strong>
+                                        {prod.name}
+                                    </strong>
+                                    <Link
+                                        to={`/admin/procuct/update/${prod._id}`}>
+                                        <span className='badge badge-warning badge-pill'>
+                                            Update
+                                        </span>
+                                        <span
+                                            onClick={() => {
+                                                destroy(prod._id);
+                                            }}
+                                            className='badge badge-danger badge-pill'>
+                                            Delete
+                                        </span>                                        
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </div>
             </div>           
         </Layout>
