d0696a7109994748ac6573d8198018a8c85c3eff
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-27 23:10:54 -0300

    Created CRUD methods for product.

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -111,3 +111,69 @@ export const updateOrderStatus = (userId, token, orderId, status) => {
         })
     );
 }
+
+export const getProducts = () => {
+    return(
+        fetch(`${API}/products`, {
+            method: "GET"       
+        })
+        .then(res => {
+            return res.json();
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    );
+}
+
+export const getProduct = (productId) => {
+    return(
+        fetch(`${API}/product/${productId}`, {
+            method: "GET"       
+        })
+        .then(res => {
+            return res.json();
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    );
+}
+
+export const updateProduct = (productId, userId, token, product) => {
+    return(
+        fetch(`${API}/product/${productId}/${userId}`, {
+            method: "PUT",
+            headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify(product)
+        })
+        .then(res => {
+            return res.json();
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    );
+}
+
+export const deleteProduct = (productId, userId, token) => {
+    return(
+        fetch(`${API}/product/${productId}/${userId}`, {
+            method: "DELETE",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(res => {
+            return res.json();
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    );
+}
